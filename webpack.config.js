54477d809508803d0c38e84c86583de21c1e6ad4
Matari7 <arimcf10@hotmail.es>
2024-05-26 12:22:10 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: './src/script.js',
+  entry: './script.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
